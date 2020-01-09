bc601dc847bc6049316b8f90a7555355a995a382
btolor <brandonotlor@yahoo.com>
2020-01-08 23:07:16 -0600

    Worked on the header styles and js...

--- JS/script.js
+++ JS/script.js
@@ -13,18 +13,55 @@ $(document).ready(function () {
 
     // Header Change Function On Scroll...
     $(window).scroll(function () {
+        // Header if window is scrolled down...
         if ($(this).scrollTop() > 70) {
             $("header").css("background","black");
             $("#header-container").css("padding","20px 0 10px");
             $("#logo-text").css("color","white");
             $(".menu-text").css("color","white");
-
+            // Hover effect for the logo
+            $("#logo-text").hover(function () {
+                // over
+                $("#logo-text").css("color","#0099ff");
+            }, function () {
+                // out
+                $("#logo-text").css("color","white");
+            }
+        );
+        // Hover effect for the menu items
+        $(".menu-text").hover(function () {
+                // over
+                $(this).css("color","#0099ff");
+            }, function () {
+                // out
+                $(this).css("color","white");
+            }
+        );
         }
+        // Header whwn window is scrolled to the top...
         if ($(this).scrollTop() < 70) {
             $("header").css("background","transparent");
             $("#header-container").css("padding","40px 0 20px");
             $("#logo-text").css("color","#5b04bf");
             $(".menu-text").css("color","#5b04bf");
+            // Hover effect for the logo
+            $("#logo-text").hover(function () {
+                    // over
+                    $("#logo-text").css("color","#0099ff");
+                }, function () {
+                    // out
+                    $("#logo-text").css("color","#5b04bf");
+                }
+            );
+            // Hover effect for the menu items
+            $(".menu-text").hover(function () {
+                    // over
+                    $(this).css("color","#0099ff");
+                }, function () {
+                    // out
+                    $(this).css("color","#5b04bf");
+                }
+            );
         }
     });
 
