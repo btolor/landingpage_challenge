316ac46ba71de2fa7df9bdbf6652fa1a183973ff
btolor <brandonotlor@yahoo.com>
2020-01-13 18:36:23 -0600

    Finished page code and started working on img optimization...

--- JS/script.js
+++ JS/script.js
@@ -15,16 +15,19 @@ $(document).ready(function () {
     $(window).scroll(function () {
         // Header if window is scrolled down...
         if ($(this).scrollTop() > 70) {
-            $("header").css("background", "black");
+            $("header").css("background", "#241d7d");
             $("#header-container").css("padding", "20px 0 10px");
+            $("#green").removeClass("scrolled");
+            $("#purple").addClass("scrolled");
             $("#logo-text").css("color", "white");
             $(".menu-text").css("color", "white");
+            $(".enroll").css({"border-color":"#60b4a1"});
             $("#open-mobile-menu").css("color", "white");
             $("#close-mobile-menu").css("color", "white");
             // Hover effect for the logo
             $("#logo-text").hover(function () {
                 // over
-                $("#logo-text").css("color", "#0099ff");
+                $("#logo-text").css("color", "#edd38c");
             }, function () {
                 // out
                 $("#logo-text").css("color", "white");
@@ -33,17 +36,27 @@ $(document).ready(function () {
             // Hover effect for the menu items
             $(".menu-text").hover(function () {
                 // over
-                $(this).css("color", "#0099ff");
+                $(this).css("color", "#edd38c");
             }, function () {
                 // out
                 $(this).css("color", "white");
             }
             );
 
+            // Hover effect for enroll btn
+            $(".enroll").hover(function () {
+                // over
+                $(".enroll").css("background", "#60b4a1 ");
+            }, function () {
+                // out
+                $(".enroll").css({"color": "white","background":"transparent","border-color":"#60b4a1"});
+            }
+            );
+
             // Hover effect for the open-mobile-menu-icon
             $("#open-mobile-menu").hover(function () {
                 // over
-                $("#open-mobile-menu").css("color", "#0099ff");
+                $("#open-mobile-menu").css("color", "#edd38c");
             }, function () {
                 // out
                 $("#open-mobile-menu").css("color", "white");
@@ -53,70 +66,100 @@ $(document).ready(function () {
             // Hover effect for the close-mobile-menu-icon
             $("#close-mobile-menu").hover(function () {
                 // over
-                $("#close-mobile-menu").css("color", "#0099ff");
+                $("#close-mobile-menu").css("color", "#edd38c");
             }, function () {
                 // out
                 $("#close-mobile-menu").css("color", "white");
             }
             );
 
-
+            // hover effect for mobile menu...
+            $("#mobile-menu").find('a').hover(function () {
+                // over
+                $(this).css({"color": "#edd38c","background":"#5680e9","border-color":"#edd38c"});
+            }, function () {
+                // out
+                $(this).css({"color": "white","background":"#2c2798","border-color":"white"});
+            }
+            );
         }
         // Header whwn window is scrolled to the top...
         if ($(this).scrollTop() < 70) {
             $("header").css("background", "transparent");
             $("#header-container").css("padding", "40px 0 20px");
-            $("#logo-text").css("color", "#5b04bf");
-            $(".menu-text").css("color", "#5b04bf");
-            $("#open-mobile-menu").css("color", "#5b04bf");
-            $("#close-mobile-menu").css("color", "#5b04bf");
+            $("#purple").removeClass("scrolled");
+            $("#green").addClass("scrolled");
+            $("#logo-text").css("color", "#2c2798");
+            $(".menu-text").css("color", "#2c2798");
+            $(".enroll").css({"border-color":"#2c2798"});
+            $("#open-mobile-menu").css("color", "#2c2798");
+            $("#close-mobile-menu").css("color", "#2c2798");
             // Hover effect for the logo
             $("#logo-text").hover(function () {
                 // over
-                $("#logo-text").css("color", "#0099ff");
+                $("#logo-text").css("color", "#60b4a1");
             }, function () {
                 // out
-                $("#logo-text").css("color", "#5b04bf");
+                $("#logo-text").css("color", "#2c2798");
             }
             );
             // Hover effect for the menu items
             $(".menu-text").hover(function () {
                 // over
-                $(this).css("color", "#0099ff");
+                $(this).css("color", "#60b4a1");
             }, function () {
                 // out
-                $(this).css("color", "#5b04bf");
+                $(this).css("color", "#2c2798");
+            }
+            );
+
+            // Hover effect for enroll btn
+            $(".enroll").hover(function () {
+                // over
+                $(".enroll").css({"background": "#2c2798","border-color":"#60b4a1"});
+            }, function () {
+                // out
+                $(".enroll").css({"color": "#2c2798","background":"transparent","border-color":"#2c2798"});
             }
             );
 
             // Hover effect for the open-mobile-menu-icon
             $("#open-mobile-menu").hover(function () {
                 // over
-                $("#open-mobile-menu").css("color", "#0099ff");
+                $("#open-mobile-menu").css("color", "#60b4a1");
             }, function () {
                 // out
-                $("#open-mobile-menu").css("color", "#5b04bf");
+                $("#open-mobile-menu").css("color", "#2c2798");
             }
             );
 
             // Hover effect for the close-mobile-menu-icon
             $("#close-mobile-menu").hover(function () {
                 // over
-                $("#close-mobile-menu").css("color", "#0099ff");
+                $("#close-mobile-menu").css("color", "#60b4a1");
             }, function () {
                 // out
-                $("#close-mobile-menu").css("color", "#5b04bf");
+                $("#close-mobile-menu").css("color", "#2c2798");
+            }
+            );
+
+            // hover effect for mobile menu...
+            $("#mobile-menu").find('a').hover(function () {
+                // over
+                $(this).css({"color": "#edd38c","background":"#5680e9","border-color":"#edd38c"});
+            }, function () {
+                // out
+                $(this).css({"color": "white","background":"#2c2798","border-color":"white"});
             }
             );
         }
     });
 
-
-    // $("#mobile-nav").children().click(function () {
-    //     $("#open-mobile-menu").css("display", "block");-
-    //     $("#close-mobile-menu").css("display", "none");
-    //     $("#mobile-nav").slideUp();
-    // });
-
+    $(document).on('click', 'a[href^="#"]', function (event) {
+        event.preventDefault();
+        $('html,body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 800);
+    });
 
 });
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # landingpage_challenge
 Landing-page for the htmlemaildeveloper.com landing-page challenge.
+
+# Images:
+https://i.postimg.cc/wv310Z7D/asap-developers.png
+https://i.postimg.cc/4Nc8xNXH/email-marketing.png
+https://i.postimg.cc/qBSbrCHd/exclusive-courses.png
+https://i.postimg.cc/QtPHPmTw/green-email-logo.png
+https://i.postimg.cc/J7G6S5gq/joe-santos-garcia.jpg
+https://i.postimg.cc/RZzg650s/programming-courses.png
+https://i.postimg.cc/CdpdMKJ4/purple-email-logo.png
+https://i.postimg.cc/x8zcSmGC/white-email-logo.png
+
